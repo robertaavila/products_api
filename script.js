aa5397628fae8b28a9bfc8ef24fd5a2f4f889e31
robertaavila <roberta.avila@sc.senai.br>
2020-10-11 22:12:06 -0300

    Inserindo cards puxando dados da API.

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
-// console.log('Starting fetch call');
-var page = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1";
-const productsPromise = fetch(page)
-// console.log('After fetch call');
-// console.log('productsPromise');
-//
-// console.log('After program has run');
+var pageNumber = 1;
 
-productsPromise
-.then(data => data.json())
-.then(data => {
-    console.log(data);
-    data.products.forEach((item) => {
-        console.log(item.name);
-    });
-})
+(function addProducts() {
+    var newPage;
+    console.log(pageNumber);
+
+    var page = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=" + pageNumber;
+
+    window.onload=function(){
+        newPage = document.getElementById('nextPage').addEventListener('click', function() {
+            document.getElementById('product_itens').innerHTML = "";
+            pageNumber += 1;
+            console.log(pageNumber);
+            addProducts();
+        });
+    }
+
+    var productsPromise = fetch(page)
+
+    productsPromise
+        .then(data => data.json())
+        .then(data => {
+            console.log(data);
+            let createItens = document.createElement('div');
+            data.products.forEach((item) => {
+                console.log(item.image);
+
+                let card = document.createElement('div');
+                card.classList.add('card');
+
+                let image = document.createElement('img');
+                image.classList.add( 'img_card');
+                image.setAttribute('src', item.image);
+                console.log(image);
+                card.appendChild(image);
+
+                let name = document.createElement('p');
+                name.innerHTML = ('Name: ' + item.name + ', ');
+                card.appendChild(name);
+
+                let oldPrice = document.createElement('p');
+                oldPrice.innerHTML = ('Old price: ' + item.oldPrice + ', ');
+                card.appendChild(oldPrice);
+
+                let price = document.createElement('p')
+                price.innerHTML = ('Price: ' + item.price);
+                card.appendChild(price);
+
+                let product_itens = document.querySelector("#product_itens");
+                product_itens.appendChild(card);
+
+            })
+            data.products.forEach((item) => {
+
+                }
+
+            )
+        });
+})();
+
+
+
+
