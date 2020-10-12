106d1272e5b0a510c65c2835ae40708a693f5982
robertaavila <roberta.avila@sc.senai.br>
2020-10-12 15:28:06 -0300

    Ajustes no css dos botões, inserção de navegação no topo da página e validação nos formulários.

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ var pageNumber = 1;
             pageNumber += 1;
             // console.log(pageNumber);
             addProducts();
+            document.getElementById('products_nextPage').scrollIntoView({block: "start", behavior: "smooth"});
         });
     }
 
@@ -28,37 +29,39 @@ var pageNumber = 1;
                 card.classList.add('card');
 
                 let image = document.createElement('img');
-                image.classList.add('img_card');
+                image.classList.add('card__img');
                 image.setAttribute('src', item.image);
+                image.setAttribute('alt', 'imagem de produto');
                 // console.log(image);
                 card.appendChild(image);
 
                 let name = document.createElement('p');
-                name.classList.add('name_card');
+                name.classList.add('card__name');
                 name.innerHTML = (item.name);
                 card.appendChild(name);
 
                 let description = document.createElement('p');
-                description.classList.add('description_card');
+                description.classList.add('card__description');
                 description.innerHTML = (item.description);
                 card.appendChild(description);
 
                 let oldPrice = document.createElement('p');
-                oldPrice.classList.add('old_price_card');
+                oldPrice.classList.add('card__old_price');
                 oldPrice.innerHTML = ('De: R$' + item.oldPrice);
                 card.appendChild(oldPrice);
 
                 let price = document.createElement('p');
-                price.classList.add('price_card');
+                price.classList.add('card__price');
                 price.innerHTML = ('Por: R$' + item.price);
                 card.appendChild(price);
 
                 let installments = document.createElement('p');
-                installments.classList.add('installments_card');
-                installments.innerHTML = ('Ou ' + item.installments.count + 'x de R$' + item.installments.value + '9');
+                installments.classList.add('card__installments');
+                installments.innerHTML = ('ou ' + item.installments.count + 'x de R$' + item.installments.value + '9');
                 card.appendChild(installments);
 
                 let button = document.createElement('button');
+                button.classList.add('card__button_buy');
                 button.innerHTML = 'Comprar';
                 card.appendChild(button);
 
@@ -66,10 +69,6 @@ var pageNumber = 1;
                 product_itens.appendChild(card);
 
             })
-            data.products.forEach((item) => {
-
-                }
-            )
         });
 })();
 
