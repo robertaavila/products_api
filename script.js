effc8cfc0c82c5bb3fea280e7df765f44dbdbe59
robertaavila <roberta.avila@sc.senai.br>
2020-10-11 23:06:43 -0300

    Ajuste no carregamento e layout dos cards.

--- script.js
+++ script.js
@@ -6,9 +6,8 @@ var pageNumber = 1;
 
     var page = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=" + pageNumber;
 
-    window.onload=function(){
-        newPage = document.getElementById('nextPage').addEventListener('click', function() {
-            document.getElementById('product_itens').innerHTML = "";
+    window.onload = function () {
+        newPage = document.getElementById('nextPage').addEventListener('click', function () {
             pageNumber += 1;
             console.log(pageNumber);
             addProducts();
@@ -29,31 +28,44 @@ var pageNumber = 1;
                 card.classList.add('card');
 
                 let image = document.createElement('img');
-                image.classList.add( 'img_card');
+                image.classList.add('img_card');
                 image.setAttribute('src', item.image);
                 console.log(image);
                 card.appendChild(image);
 
                 let name = document.createElement('p');
-                name.innerHTML = ('Name: ' + item.name + ', ');
+                name.classList.add('name_card');
+                name.innerHTML = (item.name);
                 card.appendChild(name);
 
+                let description = document.createElement('p');
+                description.classList.add('description_card');
+                description.innerHTML = (item.description);
+                card.appendChild(description);
+
                 let oldPrice = document.createElement('p');
-                oldPrice.innerHTML = ('Old price: ' + item.oldPrice + ', ');
+                oldPrice.classList.add('old_price_card');
+                oldPrice.innerHTML = ('De: R$' + item.oldPrice);
                 card.appendChild(oldPrice);
 
-                let price = document.createElement('p')
-                price.innerHTML = ('Price: ' + item.price);
+                let price = document.createElement('p');
+                price.classList.add('price_card');
+                price.innerHTML = ('Por: R$' + item.price);
                 card.appendChild(price);
 
-                let product_itens = document.querySelector("#product_itens");
+                let installments = document.createElement('p');
+                installments.classList.add('installments_card');
+                installments.innerHTML = ('Ou ' + item.installments.count + 'x de R$' + item.installments.value + '9');
+                card.appendChild(installments);
+
+                    let
+                product_itens = document.querySelector("#product_itens");
                 product_itens.appendChild(card);
 
             })
             data.products.forEach((item) => {
 
                 }
-
             )
         });
 })();
